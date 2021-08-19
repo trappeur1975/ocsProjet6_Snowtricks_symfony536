331bddde0a42b1e7d5e2712a0e3e9b2216d5d5bd
Tr@ppeur1975 <trappeur1975photmail.com>
2021-08-19 19:34:36 +0400

    integration of messages in the frontController during a "showTrick ()"

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -36,8 +36,8 @@ class FrontController extends AbstractController
 
         return $this->render('front/showTrick.html.twig', [
             'title' => 'bienvenue sur le trick',
-            'trick' => $trick,
-            'pictures' => $trick->getPictures()
+            'trick' => $trick
+            // 'pictures' => $trick->getPictures()
         ]);
     }
 }
